17f3f4d351a232d8800acc1409c3a78b47ef0600
Jivan Sharma <jivan4sharma3@gmail.com>
2025-09-29 12:30:13 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import SignIn from './Components/SignIn';
 import Login from './Components/Login';
 
